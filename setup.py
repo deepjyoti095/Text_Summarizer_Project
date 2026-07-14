e40f01f939253f57374ce965f2f0ff600a7ed6c2
Deepjyoti <deepjyoti692@gmail.com>
2026-07-14 22:38:26 +0530

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__="0.0.0"
+
+REPO_NAME = "Text_Summarizer_Project"
+AUTHOR_USER_NAME = "Deepjyoti-hub"
+SRC_REPO = "text-summarizer"
+AUTHOR_EMAIL = "deepjyoti095@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small package for Text Summarization (NLP app)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    packages=setuptools.find_packages(where="src"),
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+)
