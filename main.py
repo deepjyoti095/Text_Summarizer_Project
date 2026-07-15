a5963dab1d3655bfd7662401dea0feffd5190ba2
Deepjyoti <deepjyoti692@gmail.com>
2026-07-15 23:02:04 +0530

    Model Trainer completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textsummarizer.logging import logger
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textsummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAZE_NAME = "Data Ingestion stage"
 try:
@@ -28,6 +29,16 @@ try:
     logger.info(f">>>>> stage {STAZE_NAME} started <<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>> stage {STAZE_NAME} completed <<<<<\n\nx==========x")    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAZE_NAME = "Model Training Stage"
+try:
+    logger.info(f">>>>> stage {STAZE_NAME} started <<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>> stage {STAZE_NAME} completed <<<<<\n\nx==========x")    
 except Exception as e:
     logger.exception(e)
