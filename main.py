445f8c7ef93366322c363cc0d1347aadf46aedaf
Deepjyoti <deepjyoti692@gmail.com>
2026-07-15 18:08:19 +0530

    Data Tranformation completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textsummarizer.logging import logger
 from textsummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textsummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textsummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAZE_NAME = "Data Ingestion stage"
 try:
@@ -18,6 +19,16 @@ try:
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
     logger.info(f">>>>> stage {STAZE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAZE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>> stage {STAZE_NAME} started <<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>> stage {STAZE_NAME} completed <<<<<\n\nx==========x")    
 except Exception as e:
     logger.exception(e)
     raise e
